1667f14a2ca9d5dd74025fa8cbb86c00a00175de
Csyhd <csyhd@msn.com>
2020-12-23 16:31:56 +0800

    feat:add check cookies router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes/')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -11,8 +12,9 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(routes)
 app.use(express.static('public'))
+app.use(cookieParser())
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
--- routes/modules/login.js
+++ routes/modules/login.js
@@ -2,9 +2,20 @@ const express = require('express')
 const router = express.Router()
 const checkUser = require('../../public/javascripts/checkUser')
 
-router.post('/', (req, res) => {
+
+router.get('/', (req, res) => {
+  let userName = 'guest'
+  if (req.cookies.userName) {
+    userName = req.cookies.userName
+    return res.render('login', { userName })
+  }
+  res.render('index')
+})
+
+router.post('/home', (req, res) => {
   let userInfo = req.body
   const userName = checkUser(userInfo)
+  res.cookie('userName', userName, { maxAge: 600000 })
   switch (userName) {
     case 'NotFind':
       res.render('index', { userNotFind: '找不到使用者' })
